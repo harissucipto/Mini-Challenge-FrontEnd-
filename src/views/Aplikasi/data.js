db90b08f7dd01b6a35e273f86434161f2bc5357f
Haris Sucipto <harisssucipto@gmail.com>
2019-05-29 04:27:51 +0700

    tambah data dummy stok barang

---
+++ src/views/Aplikasi/data.js
@@ -0,0 +1,16 @@
+const stokBarang = [
+  {
+    id: 1,
+    nama: 'Daging',
+    stok: '10',
+    satuan: 'kg'
+  },
+  {
+    id: 2,
+    nama: 'Buku',
+    stok: '29',
+    satuan: 'rim'
+  }
+];
+
+export { stokBarang };
